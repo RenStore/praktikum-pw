6d5f44f8b89883e1f8f2f563ed4e6eae62a25e1f
RenStore <shandymayoraya26@gmail.com>
2024-05-09 11:09:08 +0800

    first commit

---
+++ prak2.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h2>Nilai Mahasiswa : </h2>";
+$nim = '2301010346';
+$nama = 'Shandy Mayoraya';
+$prodi = 'TI - MDI';
+$nilai = 90;
+echo "NIM : 2301010346 ", $nim, "<br>";
+echo "Nama : Shandy Mayoraya", $nama, "<br>";
+echo "Prodi : TI-MDI", $prodi, "<br>";
+echo "Nilai : $nilai";
+?>
---
+++ prak5.php
@@ -0,0 +1,9 @@
+<?php
+$username = "Shandy";
+if ($username == "Shandy") {
+echo "Anda Berhasil Login!, Selamat datang
+$username";
+} else {
+echo "Username salah, silahkan coba lagi!";
+}
+?>
